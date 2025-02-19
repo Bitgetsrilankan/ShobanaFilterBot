0418bdd18ff25908d51a651a92cd240d2a8f5581
Neoflix <roxr5650@gmail.com>
2025-02-19 08:17:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7221872059:AAE8vrOrwcuBHg2goc7lanUAxAVT67E
 BOT_START_TIME = time()
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://envs.sh/YxC.jpg https://envs.sh/YxR.jpg https://envs.sh/YxH.jpg https://envs.sh/Yxk.jpg https://envs.sh/Yxv.jpg https://envs.sh/YxU.jpg https://envs.sh/Yx4.jpg')).split()
+PICS = (environ.get('PICS', 'https://i.ibb.co/hx3pkmpm/photo-5923786806845880078-y.jpg')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1481322134').split()]
